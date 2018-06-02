821de349c644f6b37f2313656a2af7903137e30a
Aditya <adityasingh222247@gmail.com>
2018-06-02 10:09:02 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {   
+  constructor(private http: HttpClient){
+  }
   title = 'app';
+
+  ngOnInit(): void {
+    // GET
+  //   this.http.get('http://127.0.0.1:8000/snippets/').subscribe(data => {
+  //     console.log(data);
+  //   },
+  // err=>{
+  //   console.log("Error Occured");
+  // });
+  // POST
+
+  const req = this.http.post('http://127.0.0.1:8000/snippets/', {
+    title: 'foo',
+    code: 'bar',
+    Lineos: true,
+    Language:'ABAP',
+    style:'abap'
+  })
+    .subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
